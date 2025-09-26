90f385d2b5aae1c4ea9fcf4b1e2e66cd3844dc31
Eduardo-D-Vega <eduardoduartevega2006@gmail.com>
2025-09-26 12:10:28 -0600

    Ayuda

--- Practica1/Program.cs
+++ Practica1/Program.cs
@@ -1,11 +1,3 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
 Console.WriteLine("Saludos");
-public class prueba()
-{
-    
-}
-public class prueba2()
-{
-
-}
